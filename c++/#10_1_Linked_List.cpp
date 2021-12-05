663e4e1e7e00e462082554fb108c2a42938864b1
Dushyant Jakhar <44732347+dushyant0007@users.noreply.github.com>
2021-12-05 12:51:28 +0530

    Add files via upload

--- c++/#10_1_Linked_List.cpp
+++ c++/#10_1_Linked_List.cpp
@@ -3,7 +3,6 @@ using namespace std;
 
 class Node
 {
-
 public:
     int data;
     Node *next;
@@ -15,15 +14,37 @@ public:
     }
 };
 
-void printLinkdList(Node *ptr)
+void printLinkedList(Node *ptr)
 {
-
+    
     while (ptr != NULL)
     {
         ptr = ptr->next;
         cout << ptr->data << endl;
-        
     }
+}
+
+void insertAtHead(Node* head , int value){
+
+    // cout<<head<<endl;
+    // head->data = 2090;
+
+    Node *n = new Node();
+    n->data = value;
+    n->next = head->next;
+    head->next = n;
+}
+
+void insertAtEnd(Node* head , int value){
+
+    while (head->next != NULL)
+    {
+       head = head->next;
+    }
+    Node *n = new Node;
+    n->data = value;
+    head->next = n;
+
 }
 
 int main()
@@ -43,26 +64,30 @@ int main()
     two->data = 222;
     three->data = 333;
 
-    // printLinkdList(head);
+    // insertAtHead(head,1920);
+    insertAtHead(head,47);
+    // cout<<head<<endl;
+    // cout<<head->data<<endl;
+    insertAtEnd(head,66);
+    cout<<head->data<<endl;
+
+    printLinkedList(head);
 
 
+    // // ---------Doubt---------//
+    // int a = 10;
+    // int &t = a;
+    // cout << "&t = " << &t << cout;
+    // int *aPtr = &a;
+    // cout << "a -> " << *aPtr << endl;
+    // cout << aPtr << endl;
+    // cout << &a << endl;
 
-   // ---------DOUTE---------//   
-    int a = 10;
-    int &t = a;
-    cout<<"&t = "<<&t<<cout;
-    int* aPtr = &a; 
-    cout<<"a -> "<<*aPtr<<endl;
-    cout<<aPtr<<endl;
-    cout<<&a<<endl;
-    
-    Node temp;
-    Node *k = &temp;
-    cout<<"Value Of k = "<<k->data<<endl;
-    cout<<"&temp.data = "<<&temp.data<<endl;
-    cout<<"*k->data ="<<(*k).data<<endl;// *k.data and *k->data Don't work
-
-
+    // Node temp;
+    // Node *k = &temp;
+    // cout << "Value Of k = " << k->data << endl;
+    // cout << "&temp.data = " << &temp.data << endl;
+    // cout << "*k->data =" << (*k).data << endl; // *k.data and *k->data Don't work
 
     return 0;
 }
