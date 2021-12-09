57daa5e33dcceacad37a35cfce09d696b9fd927e
Dushyant Jakhar <44732347+dushyant0007@users.noreply.github.com>
2021-12-09 23:23:54 +0530

    Add files via upload

--- c++/test.cpp
+++ c++/test.cpp
@@ -5,8 +5,17 @@ int main(){
 
     int a = 1;
     int b = 3;
-    if(a!=b){
-        cout<<"good"<<endl;
-    }
+    
+    int &p = a;
+    int &q = b;
+    
+    p = 11;
+    cout<<a<<endl;
+
+    int *ptr = &a;
+
+    cout<<" "<< ptr <<" "<< *ptr <<endl;
+
+    cout<<" "<< p <<" "<< &p <<" "<< &a <<endl;
 
 }
