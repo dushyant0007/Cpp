86ef8349ad0ddbf227605e6996b2bfcbfc876ebf
Dushyant Jakhar <44732347+dushyant0007@users.noreply.github.com>
2021-12-16 16:58:15 +0530

    Add files via upload

---
+++ c++/Oops/#14_functionTemplates.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+
+template <typename Q> Q add(Q x,Q y){
+  return x+y;
+}
+
+template <typename Q,typename R> R addTow(Q x,R y){
+  return x+y;
+}
+
+int main(){
+
+ int p = add(1,2);
+ float t = add(1,2);
+ float r = add(1.4,2.4);
+ int x = add<float>(12.2,1.1);
+
+ cout<<p<<endl;
+ cout<<t<<endl;
+ cout<<r<<endl;
+ cout<<x<<endl;
+
+cout<<"---------------------"<<endl;
+
+ cout<<addTow<int,float>(12,13.5)<<endl;
+
+
+ 
+
+
+
+}
