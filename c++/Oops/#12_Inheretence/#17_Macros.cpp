64b007783c34054e007731ed3fddd923fa99b28a
Dushyant Jakhar <44732347+dushyant0007@users.noreply.github.com>
2022-09-17 13:48:22 +0530

    Add files via upload

---
+++ c++/Oops/#12_Inheretence/#17_Macros.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+#define PRINT cout<<"print"<<endl;
+#define OPEN_CURLY {
+#define LOG(x,y) cout<<x<<y<<endl;
+
+int main() 
+// { 
+OPEN_CURLY
+
+  PRINT
+  LOG(12,"GKG")
+
+
+}
